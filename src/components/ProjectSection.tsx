458dbcf155688b926fc40ce8d25e3378001e5a20
Hendrix <hoyheenlawlarlawal@gmail.com>
2026-07-20 12:48:42 +0100

    My Personal Portfolio

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -39,7 +39,7 @@ const projects = [
   {
     id: 4,
     title: 'Law Firm Website',
-    category: 'Web Design',
+    category: 'Web Development',
     image:
       '/Image/law-firm.png',
     description:
